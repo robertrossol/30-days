7ac7575a49d54481016b3673b8c39a9047dc3464
Robert Rossol <rrossol88@gmail.com>
2023-03-19 00:00:11 -0500

    making anagrams: start

---
+++ making_anagrams.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'makeAnagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING a
+#  2. STRING b
+#
+
+def makeAnagram(a, b)
+    # creates a hash of letters and their frequency from a string
+    x.each.with_object(Hash.new(0)) do |el, hash|
+      irb(main):036:1*   hash[el] += 1
+      irb(main):037:0> end
+      
+
+end
