3012e24ebb051017c62ff07579108de92a045047
Robert Rossol <rrossol88@gmail.com>
2023-03-26 11:56:00 -0500

    valid_string cleanup + optimizations

--- valid_string.rb
+++ valid_string.rb
@@ -11,12 +11,16 @@ require 'stringio'
 #
 
 def isValid(s)
-    # Write your code here
-    char_count = s.chars.each.with_object(Hash.new(0)) {|char, hsh| hsh[char] += 1}
-    lengths = char_count.values.uniq
-    lengths_count = lengths.map { |len| [len, char_count.values.count(len)]}.to_h
-    return "NO" unless (lengths.size == 1 || (lengths.size == 2 && ((lengths_count[1] == 1) || ((lengths_count[lengths.max] == 1) && (lengths.max - lengths.min == 1)))))
-    "YES"
+  char_count = s.chars.each.with_object(Hash.new(0)) {|char, hsh| hsh[char] += 1}
+
+  lengths = char_count.values.uniq
+  lengths_count = lengths.map { |len| [len, char_count.values.count(len)]}.to_h
+  # alternative way to create the lengths_count hash:
+  # char_count.values.group_by(&:itself).to_h.transform_values(&:length)
+
+  lengths.size == 1 || 
+    lengths.size == 2 && 
+      (lengths_count[1] == 1 || lengths_count[lengths.max  ] == 1 && lengths.max - lengths.min == 1) ? "YES" : "NO"
+  # instead of lengths.max - lengths.min == 1, use the absolute value of reduce(&:-) on the keys of lengths_count
+  # lengths_count.keys.reduce(&:-).abs
 end
-
-
