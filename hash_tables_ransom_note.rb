263935988bb7b77ca10aaffb8b11ca03c98c2866
Robert Rossol <rrossol88@gmail.com>
2023-04-06 18:45:33 -0500

    hash_tables_ransom_note exercise completed

---
+++ hash_tables_ransom_note.rb
@@ -0,0 +1,39 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note)
+    a = [magazine, note].map do |arr|
+       arr.each.with_object(Hash.new(0)) {|word, hash| hash[word] += 1 } 
+    end
+    
+    a[1].each do |word, count|
+        if a[0][word] < count
+            puts "No"
+            return 0
+        end
+    end
+    puts "Yes"
+end
+
+
+first_multiple_input = gets.rstrip.split
+
+m = first_multiple_input[0].to_i
+
+n = first_multiple_input[1].to_i
+
+magazine = gets.rstrip.split
+
+note = gets.rstrip.split
+
+checkMagazine magazine, note
